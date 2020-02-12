08d4eeeaee2d74098b683407b7dfcf1cf97deda0
Pirmayr <pirmayr@gmail.com>
2020-02-12 07:30:17 +0100

    Erweiterungen:   * Parameterdatei   * Aliases   * Filter

--- TemplateExpander/Expander.cs
+++ TemplateExpander/Expander.cs
@@ -1,93 +1,62 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Xml;
 
 namespace TemplateExpander
 {
-    internal class Expander
+    internal static class Expander
     {
+        private const string DefaultFilter = ".";
         private const string ExtensionTemplate = ".txt";
+        private const string NameAlias = "alias";
+        private const string NameFilter = "filter";
+        private const char SeparatorValues = '|';
         private const string ValueName = "#text";
         private const string VariableNameValue = "%value%";
 
-        public static string Expansion(string templateSet, string templatesDirectory, string xmlPath)
+        public static string Expansion(string templateSet, string templatesDirectory, XmlNode root, string parametersPath)
         {
-            var root = new XmlDocument();
-            root.LoadXml(File.ReadAllText(xmlPath));
-
-            /*
-            var specification = Directory.GetFiles(Path.GetDirectoryName(xmlPath), "*.xml");
-
-            foreach (var currentPath in specification)
-            {
-              if (!string.Equals(Path.GetFileName(currentPath), Path.GetFileName(xmlPath), StringComparison.CurrentCultureIgnoreCase))
-              {
-                var currentRoot = new XmlDocument();  
-                currentRoot.LoadXml(File.ReadAllText(currentPath));
-                if (currentRoot.DocumentElement != null)
-                {
-                  foreach (XmlNode node in currentRoot.DocumentElement.ChildNodes)
-                  {
-                    XmlNode imported = root.ImportNode(node, true);
-                    root.DocumentElement?.AppendChild(imported);
-                  }
-                }
-              }
-            }
-            */
-
-            /*
-            var compoundNodes = root.SelectNodes("/doxygenindex/compound");
-
-            if (compoundNodes != null)
-            {
-              foreach (XmlNode currentNode in  compoundNodes)
-              {
-                XmlAttribute currentRefId = currentNode.Attributes?["refid"];
-                if (currentRefId != null)
-                {
-                  string currentPath = Path.GetDirectoryName(xmlPath) + "\\" + currentRefId.Value + ".xml";
-                  if (File.Exists(currentPath))
-                  {
-                    XmlDocument currentRoot = new XmlDocument();
-                    currentRoot.LoadXml(File.ReadAllText(currentPath));
-                    if (currentRoot.DocumentElement != null)
-                    {
-                      foreach (XmlNode node in currentRoot.DocumentElement.ChildNodes)
-                      {
-                        XmlNode imported = root.ImportNode(node, true);
-                        root.DocumentElement?.AppendChild(imported);
-                      }
-                    }
-                  }
-                }
-              }
-            }
-            */
-
-            return CleanValue(Expansion(templateSet, templatesDirectory, root), false);
+            return CleanValue(Expansion(templateSet, templatesDirectory, root, ReadParameters(parametersPath)), false);
         }
 
-        private static void AddExpansion(Dictionary<string, string> expansions, string key, string value)
+        private static bool Accept(XmlNode node, Parameters parameters)
         {
+            foreach (var currentFilterValue in parameters.Get(NameFilter, node.LocalName, DefaultFilter).Split(SeparatorValues))
+            {
+                if (node.SelectSingleNode(currentFilterValue) != null)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static void AddExpansion(Dictionary<string, string> expansions, string key, string value, Parameters parameters)
+        {
+            var alias = value;
+            foreach (var currentAlias in parameters.Get(NameAlias))
+            {
+                if (currentAlias.Key == value)
+                {
+                    alias = currentAlias.Value;
+                }
+            }
             if (!expansions.ContainsKey(key))
             {
                 expansions.Add(key, "");
             }
-            expansions[key] += value;
+            expansions[key] += alias;
         }
 
-        private static void AddExpansions(string templateSet, string templatesDirectory, IEnumerable nodes, bool isValueTemplate, Strings expansions)
+        private static void AddExpansions(string templateSet, string templatesDirectory, IEnumerable nodes, bool isValueTemplate, Strings expansions, Parameters parameters)
         {
             if (nodes != null)
             {
                 foreach (XmlNode currentChild in nodes)
                 {
-                    AddExpansion(expansions, isValueTemplate ? VariableNameValue : "%" + currentChild.LocalName + "%", Expansion(templateSet, templatesDirectory, currentChild));
+                    AddExpansion(expansions, isValueTemplate ? VariableNameValue : "%" + currentChild.LocalName + "%", Expansion(templateSet, templatesDirectory, currentChild, parameters), parameters);
                 }
             }
         }
@@ -102,19 +71,52 @@ namespace TemplateExpander
             return result;
         }
 
-        private static string Expansion(string templateSet, string templatesDirectory, XmlNode node)
+        private static string Expansion(string templateSet, string templatesDirectory, XmlNode node, Parameters parameters)
         {
             var name = node.LocalName;
             if (name != ValueName)
             {
-                var template = Template(templateSet, templatesDirectory, name);
-                var isValueTemplate = template.Contains(VariableNameValue);
-                var expansions = new Strings {{VariableNameValue, ""}};
-                AddExpansions(templateSet, templatesDirectory, node.ChildNodes, isValueTemplate, expansions);
-                AddExpansions(templateSet, templatesDirectory, node.Attributes, isValueTemplate, expansions);
-                return expansions.Aggregate(template, (current, currentExpansion) => current.Replace(currentExpansion.Key, currentExpansion.Value));
+                if (Accept(node, parameters))
+                {
+                    var template = Template(templateSet, templatesDirectory, name);
+                    var isValueTemplate = template.Contains(VariableNameValue);
+                    var expansions = new Strings {{VariableNameValue, ""}};
+                    AddExpansions(templateSet, templatesDirectory, node.ChildNodes, isValueTemplate, expansions, parameters);
+                    AddExpansions(templateSet, templatesDirectory, node.Attributes, false, expansions, parameters);
+                    return expansions.Aggregate(template, (current, currentExpansion) => current.Replace(currentExpansion.Key, currentExpansion.Value));
+                }
+                return "";
             }
             return CleanValue(node.Value, true);
+        }
+
+        private static Parameters ReadParameters(string path)
+        {
+            var result = new Parameters();
+            foreach (var currentLine in File.ReadAllLines(path))
+            {
+                var currentItems = currentLine.Split(';');
+                if (currentItems.Length == 3)
+                {
+                    var currentName = currentItems[0];
+                    var currentKey = currentItems[1];
+                    var currentValue = currentItems[2];
+                    if (!result.ContainsKey(currentName))
+                    {
+                        result.Add(currentName, new Strings());
+                    }
+                    if (!result[currentName].ContainsKey(currentKey))
+                    {
+                        result[currentName].Add(currentKey, "");
+                    }
+                    if (!string.IsNullOrEmpty(result[currentName][currentKey]))
+                    {
+                        result[currentName][currentKey] += SeparatorValues;
+                    }
+                    result[currentName][currentKey] += currentValue;
+                }
+            }
+            return result;
         }
 
         private static string ReplaceExhaustive(string value, string searchString, string replacement)
@@ -137,6 +139,26 @@ namespace TemplateExpander
         {
             var path = templatesDirectory + templateSet + "." + tag + ExtensionTemplate;
             return File.Exists(path) ? File.ReadAllText(path) : VariableNameValue;
+        }
+
+        private class Parameters : Dictionary<string, Strings>
+        {
+            public string Get(string name, string key, string defaultValue)
+            {
+                if (TryGetValue(name, out var section))
+                {
+                    if (section.TryGetValue(key, out var result))
+                    {
+                        return result;
+                    }
+                }
+                return defaultValue;
+            }
+
+            public Strings Get(string name)
+            {
+                return TryGetValue(name, out var result) ? result : new Strings();
+            }
         }
 
         private class Strings : Dictionary<string, string>
--- TemplateExpander/Program.cs
+++ TemplateExpander/Program.cs
@@ -1,13 +1,54 @@
 ﻿using System.IO;
+using System.Xml;
 
 namespace TemplateExpander
 {
     internal static class Program
     {
-        private static void Main()
+        private static void AddDoxygenCompounds(string xmlPath, XmlDocument root)
         {
-            var expansion = Expander.Expansion("md", @"c:/usr/prg/TemplateExpander/TemplateExpander/Templates/", @"C:\Entwicklung\branches\B5010\P4\Source\chiemsee\Components\ObjectViewer\Doc\xml\all.xml");
-            File.WriteAllText(@"c:/usr/prg/TemplateExpander/TemplateExpander/all.md", expansion);
+            var selectedNodes = root.SelectNodes("/doxygenindex/compound");
+            if (selectedNodes != null)
+            {
+                foreach (XmlNode currentNode in selectedNodes)
+                {
+                    var currentRefId = currentNode.Attributes?["refid"];
+                    if (currentRefId != null)
+                    {
+                        var currentPath = Path.GetDirectoryName(xmlPath) + "/" + currentRefId.Value + ".xml";
+                        if (File.Exists(currentPath))
+                        {
+                            var currentRoot = new XmlDocument();
+                            currentRoot.LoadXml(File.ReadAllText(currentPath));
+                            if (currentRoot.DocumentElement != null)
+                            {
+                                foreach (XmlNode node in currentRoot.DocumentElement.ChildNodes)
+                                {
+                                    root.DocumentElement?.AppendChild(root.ImportNode(node, true));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void Main(string[] arguments)
+        {
+            var templateSet = arguments[0];
+            var templatesDirectory = arguments[1];
+            var xmlPath = arguments[2];
+            var outputPath = arguments[3];
+            var parametersPath = arguments[4];
+            File.WriteAllText(outputPath, Expander.Expansion(templateSet, templatesDirectory, ReadXml(xmlPath), parametersPath));
+        }
+
+        private static XmlDocument ReadXml(string xmlPath)
+        {
+            var root = new XmlDocument();
+            root.LoadXml(File.ReadAllText(xmlPath));
+            AddDoxygenCompounds(xmlPath, root);
+            return root;
         }
     }
 }
