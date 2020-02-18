918b32902274df9c33be9a706e1a937eef059ed1
Pirmayr <pirmayr@gmail.com>
2020-02-18 13:21:06 +0100

    Templates improved further.

--- TemplateExpander/Parameters.cs
+++ TemplateExpander/Parameters.cs
@@ -3,59 +3,69 @@ using System.IO;
 
 namespace TemplateExpander
 {
-    public class Parameters : Dictionary<string, Strings>
+  public class Parameters : Dictionary<string, Strings>
+  {
+    public const char SeparatorValues = '|';
+
+    public static Parameters ReadParameters(string path, string target, string format)
     {
-        public const char SeparatorValues = '|';
-
-        public static Parameters ReadParameters(string path)
+      Parameters result = new Parameters();
+      foreach (string currentLine in File.ReadAllLines(path))
+      {
+        string[] currentItems = currentLine.Split(';');
+        if (currentItems.Length == 5)
         {
-            var result = new Parameters();
-            foreach (var currentLine in File.ReadAllLines(path))
+          string currentTarget = currentItems[0];
+          string currentFormat = currentItems[1];
+          if (Accept(target, currentTarget, format, currentFormat))
+          {
+            string currentName = currentItems[2];
+            string currentKey = currentItems[3];
+            string currentValue = currentItems[4];
+            if (!result.ContainsKey(currentName))
             {
-                var currentItems = currentLine.Split(';');
-                if (3 <= currentItems.Length && currentItems.Length <= 4)
-                {
-                    var currentName = currentItems[0];
-                    var currentKey = currentItems[1];
-                    var currentValue = currentItems[2];
-                    if (!result.ContainsKey(currentName))
-                    {
-                        result.Add(currentName, new Strings());
-                    }
-                    if (!result[currentName].ContainsKey(currentKey))
-                    {
-                        result[currentName].Add(currentKey, "");
-                    }
-                    if (!string.IsNullOrEmpty(result[currentName][currentKey]))
-                    {
-                        result[currentName][currentKey] += SeparatorValues;
-                    }
-                    result[currentName][currentKey] += currentValue;
-                }
+              result.Add(currentName, new Strings());
             }
-            return result;
-        }
-
-        public string Get(string name, string key, string defaultValue)
-        {
-            if (TryGetValue(name, out var section))
+            if (!result[currentName].ContainsKey(currentKey))
             {
-                if (section.TryGetValue(key, out var result))
-                {
-                    return result;
-                }
+              result[currentName].Add(currentKey, "");
             }
-            return defaultValue;
+            if (!string.IsNullOrEmpty(result[currentName][currentKey]))
+            {
+              result[currentName][currentKey] += SeparatorValues;
+            }
+            result[currentName][currentKey] += currentValue;
+          }
         }
-
-        public Strings Get(string name)
-        {
-            return TryGetValue(name, out var result) ? result : new Strings();
-        }
-
-        public string[] GetValues(string name, string key, string defaultValue)
-        {
-            return Get(name, key, defaultValue).Split(SeparatorValues);
-        }
+      }
+      return result;
     }
+
+    public static bool Accept(string target, string currentTarget, string format, string currentFormat)
+    {
+      return (currentTarget == "*" || target == currentTarget) && (currentFormat == "*" || format == currentFormat);
+    }
+
+    public string Get(string name, string key, string defaultValue)
+    {
+      if (TryGetValue(name, out Strings section))
+      {
+        if (section.TryGetValue(key, out string result))
+        {
+          return result;
+        }
+      }
+      return defaultValue;
+    }
+
+    public Strings Get(string name)
+    {
+      return TryGetValue(name, out Strings result) ? result : new Strings();
+    }
+
+    public string[] GetValues(string name, string key, string defaultValue)
+    {
+      return Get(name, key, defaultValue).Split(SeparatorValues);
+    }
+  }
 }
--- TemplateExpander/Program.cs
+++ TemplateExpander/Program.cs
@@ -1,96 +1,117 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Xml;
 
 namespace TemplateExpander
 {
-    internal static class Program
+  internal static class Program
+  {
+    private static void AddDoxygenCompounds(string xmlPath, XmlDocument root)
     {
-        private static void AddDoxygenCompounds(string xmlPath, XmlDocument root)
+      XmlNodeList selectedNodes = root.SelectNodes("/doxygenindex/compound");
+      if (selectedNodes != null)
+      {
+        foreach (XmlNode currentNode in selectedNodes)
         {
-            var selectedNodes = root.SelectNodes("/doxygenindex/compound");
-            if (selectedNodes != null)
+          XmlAttribute currentRefId = currentNode.Attributes?["refid"];
+          if (currentRefId != null)
+          {
+            string currentPath = Path.GetDirectoryName(xmlPath) + "/" + currentRefId.Value + ".xml";
+            if (File.Exists(currentPath))
             {
-                foreach (XmlNode currentNode in selectedNodes)
+              XmlDocument currentRoot = new XmlDocument();
+              currentRoot.LoadXml(File.ReadAllText(currentPath));
+              if (currentRoot.DocumentElement != null)
+              {
+                foreach (XmlNode node in currentRoot.DocumentElement.ChildNodes)
                 {
-                    var currentRefId = currentNode.Attributes?["refid"];
-                    if (currentRefId != null)
-                    {
-                        var currentPath = Path.GetDirectoryName(xmlPath) + "/" + currentRefId.Value + ".xml";
-                        if (File.Exists(currentPath))
-                        {
-                            var currentRoot = new XmlDocument();
-                            currentRoot.LoadXml(File.ReadAllText(currentPath));
-                            if (currentRoot.DocumentElement != null)
-                            {
-                                foreach (XmlNode node in currentRoot.DocumentElement.ChildNodes)
-                                {
-                                    root.DocumentElement?.AppendChild(root.ImportNode(node, true));
-                                }
-                            }
-                        }
-                    }
+                  root.DocumentElement?.AppendChild(root.ImportNode(node, true));
                 }
+              }
             }
+          }
         }
-
-        private static void Main(string[] arguments)
-        {
-            try
-            {
-                var targets = arguments[0].Split('|');
-                var optionsDirectory = arguments[1];
-                foreach (var currentTarget in targets)
-                {
-                    var currentOptionsPath = optionsDirectory + currentTarget + ".txt";
-                    var parameters = Parameters.ReadParameters(currentOptionsPath);
-                    var formats = parameters.GetValues("option", "format", "");
-                    foreach (var currentFormat in formats)
-                    {
-                        var templatesDirectory = optionsDirectory + parameters.Get("option", "templates-directory", "");
-                        var xmlPath = optionsDirectory + parameters.Get("option", "xml-path", "");
-                        var outputDirectory = optionsDirectory + parameters.Get("option", "output-directory", "");
-                        var preCommandPath = parameters.Get("option", "pre-command-path", "");
-                        var preCommandParameter0 = parameters.Get("option", "pre-command-parameter-0", "");
-                        var preCommandParameter1 = parameters.Get("option", "pre-command-parameter-1", "");
-                        var preCommandArguments = string.Format(parameters.Get("option", "pre-command-arguments", ""), preCommandParameter0, preCommandParameter1);
-                        var postCommandPath = parameters.Get("option", "post-command-path", "");
-                        var postCommandParameter0 = parameters.Get("option", "post-command-parameter-0", "");
-                        var postCommandParameter1 = parameters.Get("option", "post-command-parameter-1", "");
-                        var postCommandArguments = string.Format(parameters.Get("option", "post-command-arguments", ""), postCommandParameter0, postCommandParameter1);
-                        preCommandPath = File.Exists(optionsDirectory + preCommandPath) ? optionsDirectory + preCommandPath : preCommandPath;
-                        postCommandPath = File.Exists(optionsDirectory + postCommandPath) ? optionsDirectory + postCommandPath : postCommandPath;
-                        Directory.SetCurrentDirectory(optionsDirectory);
-                        if (!string.IsNullOrEmpty(preCommandPath))
-                        {
-                            Directory.SetCurrentDirectory(optionsDirectory);
-                            Process.Start(preCommandPath, preCommandArguments)?.WaitForExit();
-                        }
-                        var outputPath = outputDirectory + Path.GetFileNameWithoutExtension(currentTarget) + "." + currentFormat;
-                        File.WriteAllText(outputPath, Expander.Expansion(currentFormat, templatesDirectory, ReadXml(xmlPath), parameters));
-                        if (!string.IsNullOrEmpty(postCommandPath))
-                        {
-                            Directory.SetCurrentDirectory(optionsDirectory);
-                            Process.Start(postCommandPath, postCommandArguments)?.WaitForExit();
-                        }
-                    }
-                    Environment.Exit(0);
-                }
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine(exception.Message);
-                Environment.Exit(1);
-            }
-        }
-
-        private static XmlDocument ReadXml(string xmlPath)
-        {
-            var root = new XmlDocument();
-            root.LoadXml(File.ReadAllText(xmlPath));
-            AddDoxygenCompounds(xmlPath, root);
-            return root;
-        }
+      }
     }
+
+    private static void ExecuteProgram(string command, string arguments, string workingDirectory)
+    {
+      Directory.SetCurrentDirectory(workingDirectory);
+      Process process = new Process();
+      process.StartInfo.FileName = command;
+      process.StartInfo.Arguments = arguments;
+      process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+      process.StartInfo.UseShellExecute = false;
+      process.StartInfo.RedirectStandardOutput = true;
+      process.OutputDataReceived += OnDataReceived;
+      process.ErrorDataReceived += OnDataReceived;
+      process.Start();
+      process.BeginOutputReadLine();
+      process.WaitForExit();
+      process.WaitForExit();
+      process.Close();
+    }
+
+    private static void Main(string[] arguments)
+    {
+      try
+      {
+        string[] targets = arguments[0].Split(';');
+        string[] formats = arguments[1].Split(';');
+        string optionsDirectory = arguments[2];
+        foreach (string currentTarget in targets)
+        {
+          foreach (string currentFormat in formats)
+          {
+            string currentOptionsPath = optionsDirectory + "Parameters.txt";
+            Parameters parameters = Parameters.ReadParameters(currentOptionsPath, currentTarget, currentFormat);
+            string templatesDirectory = optionsDirectory + parameters.Get("option", "templates-directory", "");
+            string xmlPath = optionsDirectory + parameters.Get("option", "xml-path", "");
+            string outputDirectory = optionsDirectory + parameters.Get("option", "output-directory", "");
+            string preCommandPath = parameters.Get("option", "pre-command-path", "");
+            string preCommandArguments = parameters.Get("option", "pre-command-arguments", "").Replace("%target%", currentTarget).Replace("%format%", currentFormat);
+            string postCommandPath = parameters.Get("option", "post-command-path", "");
+            string postCommandArguments = parameters.Get("option", "post-command-arguments", "").Replace("%target%", currentTarget).Replace("%format%", currentFormat);
+            preCommandPath = File.Exists(optionsDirectory + preCommandPath) ? optionsDirectory + preCommandPath : preCommandPath;
+            preCommandPath = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? preCommandPath : Path.ChangeExtension(preCommandPath, null);
+            postCommandPath = File.Exists(optionsDirectory + postCommandPath) ? optionsDirectory + postCommandPath : postCommandPath;
+            postCommandPath = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? postCommandPath : Path.ChangeExtension(postCommandPath, null);
+            Directory.SetCurrentDirectory(optionsDirectory);
+            if (!string.IsNullOrEmpty(preCommandPath))
+            {
+              ExecuteProgram(preCommandPath, preCommandArguments, optionsDirectory);
+            }
+            string outputPath = outputDirectory + Path.GetFileNameWithoutExtension(currentTarget) + "." + currentFormat;
+            File.WriteAllText(outputPath, Expander.Expansion(currentFormat, templatesDirectory, ReadXml(xmlPath), parameters));
+            if (!string.IsNullOrEmpty(postCommandPath))
+            {
+              ExecuteProgram(postCommandPath, postCommandArguments, outputDirectory);
+              ExecuteProgram(postCommandPath, postCommandArguments, outputDirectory);
+            }
+          }
+        }
+        Environment.Exit(0);
+      }
+      catch (Exception exception)
+      {
+        Console.WriteLine(exception.Message);
+        Environment.Exit(1);
+      }
+    }
+
+    private static void OnDataReceived(object sender, DataReceivedEventArgs e)
+    {
+      Console.WriteLine(e.Data);
+    }
+
+    private static XmlDocument ReadXml(string xmlPath)
+    {
+      XmlDocument root = new XmlDocument();
+      root.LoadXml(File.ReadAllText(xmlPath));
+      AddDoxygenCompounds(xmlPath, root);
+      return root;
+    }
+  }
 }
